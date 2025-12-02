33f8704f9b5ff928ed42ce4f854a9a652b70ee52
Saff9 <saffanakbar942@gmail.com>
2025-12-02 19:07:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
+const svelte = require('@sveltejs/vite-plugin-svelte');
 
-export default defineConfig({
+module.exports = {
   plugins: [svelte()],
   build: {
     outDir: 'dist',
   },
-});
+};
