117e38e8b7002643cf150004b19c9d06bfa4cc76
Saff9 <saffanakbar942@gmail.com>
2025-12-02 19:52:58 +0530

    Create App.svelte

---
+++ frontend/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = "Svelte App";
+</script>
+
+<main>
+  <h1>Welcome to {name}</h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin-top: 40px;
+    font-family: Arial, sans-serif;
+  }
+</style>
