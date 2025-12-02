af305046bc71988d7075724de5481ccadd874811
Saff9 <saffanakbar942@gmail.com>
2025-12-02 21:35:15 +0530

    Update svelte.config.js

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,15 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const config = {
-  preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter(),
-    alias: {
-      $components: 'src/components',
-      $utils: 'src/lib/utils'
-    }
+    outDir: 'build'
   }
 };
 
